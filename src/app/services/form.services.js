af0e945533a5eec9e97bd192541803555e9a7d9f
Андрей Семин <glazunov.99@list.ru>
2023-06-18 17:02:18 +0300

    adding customization in the create form

--- src/app/services/form.services.js
+++ src/app/services/form.services.js
@@ -1,11 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
-const apiEndpoint = "https://api.sbercloud.ru/content/v1/bootcamp/frontend"
+const apiEndpoint = "https://api.sbercloud.ru/content/v1/bootcamp/frontend";
 
 const apiServices = {
-    post: async (payload) => {
-        const {data} = await axios.post(apiEndpoint, payload)
-        return data
-    }
-}
-export default apiServices
+  post: async (payload) => {
+    const { data } = await axios.post(apiEndpoint, payload);
+    return data;
+  },
+};
+export default apiServices;
